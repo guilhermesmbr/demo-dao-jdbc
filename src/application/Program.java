1992cc16867607ecaec22c18ec47f7b75e2e181a
Guilherme <guilherme-mk27@hotmail.com>
2025-09-25 14:52:26 -0300

    Seller entity class

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(1990, 5, 20), 3000.00, obj);
+        System.out.println(seller);
+    }
+}
